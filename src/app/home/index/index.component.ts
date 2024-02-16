c16a7febe9a30f0174f008f76da39ddb91c77de4
Juan David Ruiz Olmos <juanda_201001@hotmail.com>
2024-02-15 22:52:44 -0500

    information electromagnetism added

--- src/app/home/index/index.component.ts
+++ src/app/home/index/index.component.ts
@@ -10,27 +10,21 @@ import Swal from 'sweetalert2';
 export class IndexComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
+    const currentLanguage = localStorage.getItem('selectedLanguage');
+    const confurmedTitle = currentLanguage === 'es' ? "Esto es mundoSteam!" : "This is MundoSteam!";
+    const confirmedText = currentLanguage === 'es' ? "Esta página ha sido creada con el fin de compartir conocimiento y experiencias, de una forma agradable para todos los que la visitan" : "This page has been created in order to share knowledge and experiences, in a pleasant way for all who visit it.";
+
+
     $(window).on('scroll', this.handleScroll);
-    if(localStorage.getItem('selectedLanguage')=="es"){
-      Swal.fire({
-        title:"Esto es mundoSteam!",
-        text:"Esta página ha sido creada con el fin de compartir conocimiento y experiencias, de una forma agradable para todos los que la visitan",
-        imageUrl:"https://img.freepik.com/free-vector/flat-children-back-school_52683-42315.jpg?w=740&t=st=1706415993~exp=1706416593~hmac=6f2dc21cfe56635e16b191117d3c12e227d97859d4b54b6fdc1ad20e96409805",
-        imageHeight: 200,
-        imageAlt: "Students"
-      });
-    }else{
-      Swal.fire({
-        title:"This is MundoSteam!",
-        text:"This page has been created in order to share knowledge and experiences, in a pleasant way for all who visit it.",
-        imageUrl:"https://img.freepik.com/free-vector/flat-children-back-school_52683-42315.jpg?w=740&t=st=1706415993~exp=1706416593~hmac=6f2dc21cfe56635e16b191117d3c12e227d97859d4b54b6fdc1ad20e96409805",
-        imageHeight: 200,
-        imageAlt: "Students"
-      });
-    }
 
-
-          }
+    Swal.fire({
+      title: confurmedTitle,
+      text: confirmedText,
+      imageUrl:"https://img.freepik.com/free-vector/flat-children-back-school_52683-42315.jpg?w=740&t=st=1706415993~exp=1706416593~hmac=6f2dc21cfe56635e16b191117d3c12e227d97859d4b54b6fdc1ad20e96409805",
+      imageHeight: 200,
+      imageAlt: "Students"
+    })
+  }
 
   ngOnDestroy() {
     $(window).off('scroll', this.handleScroll);
