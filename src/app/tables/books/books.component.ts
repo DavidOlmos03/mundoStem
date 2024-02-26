35b1ed8d51b9e5cd8faf36830fde5e124b51c495
Juan David Ruiz Olmos <juanda_201001@hotmail.com>
2024-02-26 00:37:54 -0500

    edited books view and added access control

--- src/app/tables/books/books.component.ts
+++ src/app/tables/books/books.component.ts
@@ -1,14 +1,25 @@
 
-import { Component, EventEmitter, Output } from '@angular/core';
-
-
+import { Component, EventEmitter, Output, OnInit,OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-books-grid',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.css']
+  styleUrls: ['./books.component.css', './books.component.scss']
 })
-export class BooksGridComponent {
+export class BooksGridComponent implements OnInit,OnDestroy {
+
+  constructor(private http: HttpClient, private router: Router) { }
+  /**
+   * Se verifica que el usuario no este logueado para que pueda acceder al login
+   */
+  logueadoFunction() {
+    return localStorage.getItem('acceso');
+  }
+
+
   /**
    * Para el contenedor lateral
    */
@@ -25,16 +36,33 @@ export class BooksGridComponent {
     }
   }
 
-  /**
-   * Para controlar el titulo del header
-   */
 
-  /*
-  lateralVisible:boolean = true;
-  toggleLateral(): void {
-    this.lateralVisible = !this.lateralVisible;
+
+  ngOnInit() {
+    /**
+     * Para controlar el scroll en en header y este se oculte
+     */
+    $(window).on('scroll', this.handleScroll);
+    /**
+     * Desde que se inicia la página se verifica si el usuario esta logueado o no
+     */
+    if (!this.logueadoFunction()) {
+      this.router.navigate(['']);
+    }
   }
-   */
 
+  ngOnDestroy() {
+    $(window).off('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    const winTop: number = $(window).scrollTop() || 0;
+
+    if (winTop >= 30) {
+      $('body').addClass('sticky-header');
+    } else {
+      $('body').removeClass('sticky-header');
+    }
+  }
 
 }
