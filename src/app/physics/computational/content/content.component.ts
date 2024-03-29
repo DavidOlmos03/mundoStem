6d48e8f7db7a12bb977aaa6afd62c88d009ddf46
Juan David Ruiz Olmos <juanda_201001@hotmail.com>
2024-03-29 02:48:10 -0500

    responsive and content added

--- src/app/physics/computational/content/content.component.ts
+++ src/app/physics/computational/content/content.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class ContentComputationalComponent {
   selectedPart:string = "1940-1950";
+  hovered: number | null = null;
+  texts: string[] = [
+    "Colossus de Reino Unido",
+    "ENIAC de Estados Unidos",
+    "Libro de Juan-Antonio Infante y José María Rey",
+    "Simulación del comportamiento de un fluido",
+    "High-level programming language that has played a pivotal role in the development of scientific and engineering applications",
+    "The first proton-lead collisions of 2013 send showers of particles through the ALICE detector (Image: CERN)"
+  ];
+
+  onMouseEnter(index: number) {
+    this.hovered = index;
+  }
+
+  onMouseLeave() {
+    this.hovered = null;
+  }
 }
